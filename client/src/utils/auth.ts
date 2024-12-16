4a33077e65c1a45289a9963b4394fafb0e7e4a64
Pink727 <doc72789@gmail.com>
2024-12-16 14:10:16 -0800

    Refactor AuthService methods for token handling and user authentication

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,30 +1,43 @@
-import { JwtPayload, jwtDecode } from 'jwt-decode';
+import jwtDecode from 'jwt-decode';
 
 class AuthService {
+  // TODO: implement getProfile method
   getProfile() {
-    // TODO: return the decoded token
+    const token = this.getToken();
+    return token ? jwtDecode(token) : null;
   }
 
+  // TODO: implement loggedIn method
   loggedIn() {
-    // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
   }
-  
+
+  // TODO: implement isTokenExpired method
   isTokenExpired(token: string) {
-    // TODO: return a value that indicates if the token is expired
+    try {
+      const decoded: any = jwtDecode(token);
+      return decoded.exp < Date.now() / 1000;
+    } catch (error) {
+      return false;
+    }
   }
 
+  // TODO: implement getToken method
   getToken(): string {
-    // TODO: return the token
+    return localStorage.getItem('id_token') || '';
   }
 
+  // TODO: implement login method
   login(idToken: string) {
-    // TODO: set the token to localStorage
-    // TODO: redirect to the home page
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
   }
 
+  // TODO: implement logout method
   logout() {
-    // TODO: remove the token from localStorage
-    // TODO: redirect to the login page
+    localStorage.removeItem('id_token');
+    window.location.assign('/login');
   }
 }
 
